ba9903d53b21d87b9617687a5ae28263e0ded622
Johannes Wilm <mail@johanneswilm.org>
2019-09-24 14:55:57 +0200

    remove console.log

--- payment/static/js/modules/payment/prelogin.js
+++ payment/static/js/modules/payment/prelogin.js
@@ -1,11 +1,9 @@
 export class PaymentPrelogin {
     constructor({page}) {
-        console.log('sraert')
         this.preloginPage = page
     }
 
     init() {
-        console.log('dedede')
         this.preloginPage.footerLinks.push(
             {
                 text: gettext("Pricing"),
