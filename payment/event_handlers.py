03b2eaa8e2f4461c75352a4300b86372f112e9ab
Johannes Wilm <mail@johanneswilm.org>
2019-12-15 22:20:34 +0100

    fix customer filter

--- payment/event_handlers.py
+++ payment/event_handlers.py
@@ -9,31 +9,6 @@ from document.models import Document
 from .models import  Customer
 
 
-# @webhooks.handler("checkout.session.completed")
-# def session_succeeded(event):
-#     """Handle the session succeeded event to hook a newly created customer to
-#     a django user."""
-#     session_data = event.data.get("object", {})
-#     user_id = session_data.get("client_reference_id", None)
-#     subscription_id = session_data.get("subscription", None)
-#
-#     if subscription_id and user_id:
-#         subscription = Subscription.objects.filter(id=subscription_id).first()
-#         user = User.objects.filter(id=user_id).first()
-#         if (
-#             user and
-#             subscription and
-#             subscription.customer and
-#             not subscription.customer.subscriber
-#         ):
-#             customer = subscription.customer
-#             customer.subscriber_id = user_id
-#             customer.metadata = {} or customer.metadata
-#             customer.metadata['djstripe_subscriber'] = user_id
-#             customer.metadata['updated'] = datetime.datetime.now()
-#             customer.save()
-
-
 @receiver(post_save, sender=Document)
 def handler_save_document(sender, instance, created, **kwargs):
     if created:
@@ -44,7 +19,7 @@ def handler_save_document(sender, instance, created, **kwargs):
             forbidden = False
         else:
             customer = Customer.objects.filter(
-                subscriber=instance.owner
+                user=instance.owner
             ).first()
             if not customer:
                 pass
