b8b722a3d49b24d2e2aff37c863161cc388cb210
Johannes Wilm <mail@johanneswilm.org>
2022-12-30 18:36:49 +0100

    lint

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from glob import glob
 from setuptools import find_namespace_packages, setup
 from setuptools.command.build_py import build_py as _build_py
 
+
 def read(name):
-    with open(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
-    ) as f:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), name)) as f:
         return f.read()
+
 
 # From https://github.com/pypa/setuptools/pull/1574
 class build_py(_build_py):
@@ -29,39 +29,36 @@ class build_py(_build_py):
                     modules.remove(module)
         return modules
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='fiduswriter-payment-paddle',
-    version='3.11.1',
+    name="fiduswriter-payment-paddle",
+    version="3.11.1",
     packages=find_namespace_packages(),
-    exclude_package_data={
-        "": ["configuration.py", "django-admin.py", "build/*"]
-    },
+    exclude_package_data={"": ["configuration.py", "django-admin.py", "build/*"]},
     include_package_data=True,
-    license='AGPL License',
-    description=(
-        'A Fidus Writer plugin to allow payment through Paddle'
-    ),
+    license="AGPL License",
+    description=("A Fidus Writer plugin to allow payment through Paddle"),
     long_description=README,
-    url='https://www.github.org/fiduswriter/fiduswriter-payment-paddle',
-    author='Johannes Wilm',
-    author_email='johannes@fiduswriter.org',
+    url="https://www.github.org/fiduswriter/fiduswriter-payment-paddle",
+    author="Johannes Wilm",
+    author_email="johannes@fiduswriter.org",
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 4.1',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 4.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    cmdclass={'build_py': build_py},
-    install_requires=read('requirements.txt').splitlines(),
+    cmdclass={"build_py": build_py},
+    install_requires=read("requirements.txt").splitlines(),
 )
