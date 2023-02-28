b94a2289797d8ff120e528abb43143b7b2a8810e
Johannes Wilm <mail@johanneswilm.org>
2023-02-28 12:42:23 +0100

    mark description as markdown

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     license="AGPL License",
     description=("A Fidus Writer plugin to allow payment through Paddle"),
     long_description=README,
+    long_description_content_type="text/markdown",
     url="https://www.github.com/fiduswriter/fiduswriter-payment-paddle",
     author="Johannes Wilm",
     author_email="johannes@fiduswriter.org",
