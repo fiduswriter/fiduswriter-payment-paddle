0390799d7e9dcea2fb5929bd47d9d00cc60af773
Johannes Wilm <mail@johanneswilm.org>
2026-05-30 17:36:38 +0200

    update testing framework

--- fiduswriter/payment/tests/test_payment.py
+++ fiduswriter/payment/tests/test_payment.py
@@ -8,7 +8,7 @@ from urllib.parse import urljoin
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from channels.testing import ChannelsLiveServerTestCase
+from testing.live_server import ChannelsLiveServerTestCase
 from testing.selenium_helper import SeleniumHelper
 
 from django.conf import settings
