2ed5d03e3d7bb3a7261377631cb3b4543a8f1fd3
Johannes Wilm <mail@johanneswilm.org>
2026-08-12 15:17:32 +0200

    Block import when free document limit is reached, same as new document

--- fiduswriter/payment/static/js/modules/payment/documents_overview.js
+++ fiduswriter/payment/static/js/modules/payment/documents_overview.js
@@ -5,35 +5,58 @@ export class PaymentDocumentsOverview {
         this.overview = overview
     }
 
+    canCreateDocument() {
+        const sub = this.overview.app.subscription
+        return (
+            sub &&
+            (sub.staff ||
+                sub.subscribed ||
+                this.overview.documentList.length < 2)
+        )
+    }
+
+    showSubscriptionDialog() {
+        const dialog = new Dialog({
+            title: gettext("Subscription warning"),
+            body: `<p>${gettext("You have run out of free documents. Sign up for a subscription to create more documents.")}</p>`,
+            buttons: [
+                {
+                    text: gettext("Go to subscription page"),
+                    classes: "fw-dark",
+                    click: () => {
+                        dialog.close()
+                        this.overview.app.goTo("/payment/")
+                    }
+                },
+                {type: "close"}
+            ]
+        })
+        dialog.open()
+    }
+
     init() {
         this.overview.goToNewDocumentAction = this.overview.goToNewDocument
         this.overview.goToNewDocument = (...args) => {
             this.overview.app.getSubscription().then(() => {
-                if (
-                    this.overview.app.subscription.staff ||
-                    this.overview.app.subscription.subscribed ||
-                    this.overview.documentList.length < 2
-                ) {
+                if (this.canCreateDocument()) {
                     this.overview.goToNewDocumentAction(...args)
                 } else {
-                    const dialog = new Dialog({
-                        title: gettext("Subscription warning"),
-                        body: `<p>${gettext("You have run out of free documents. Sign up for a subscription to create more documents.")}</p>`,
-                        buttons: [
-                            {
-                                text: gettext("Go to subscription page"),
-                                classes: "fw-dark",
-                                click: () => {
-                                    dialog.close()
-                                    this.overview.app.goTo("/payment/")
-                                }
-                            },
-                            {type: "close"}
-                        ]
-                    })
-                    dialog.open()
+                    this.showSubscriptionDialog()
                 }
             })
         }
+
+        this.overview.importDocumentAction =
+            this.overview.mod.actions.importDocument.bind(
+                this.overview.mod.actions
+            )
+        this.overview.mod.actions.importDocument = (...args) =>
+            this.overview.app.getSubscription().then(() => {
+                if (this.canCreateDocument()) {
+                    return this.overview.importDocumentAction(...args)
+                } else {
+                    this.showSubscriptionDialog()
+                }
+            })
     }
 }
